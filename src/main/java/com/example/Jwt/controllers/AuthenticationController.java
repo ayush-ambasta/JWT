c081065940b2c9d780413166422de40cc5cd77f8
Ayush Ambasta <ayushambasta2001@gmail.com>
2024-04-05 21:41:27 +0530

    implementation of jwt

---
+++ src/main/java/com/example/Jwt/controllers/AuthenticationController.java
@@ -0,0 +1,29 @@
+package com.example.Jwt.controllers;
+
+import com.example.Jwt.dto.JwtAuthenticationResponse;
+import com.example.Jwt.dto.SignInRequest;
+import com.example.Jwt.dto.SignUpRequest;
+import com.example.Jwt.services.AuthenticationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+    private final AuthenticationService authenticationService;
+
+    @PostMapping("/signup")
+    public JwtAuthenticationResponse signup(@RequestBody SignUpRequest request) {
+        return authenticationService.signup(request);
+    }
+
+    @PostMapping("/signin")
+    public JwtAuthenticationResponse signin(@RequestBody SignInRequest request) {
+        return authenticationService.signin(request);
+    }
+}
---
+++ src/main/java/com/example/Jwt/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.Jwt.repositories;
+
+import com.example.Jwt.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+    Optional<User>findByEmail(String email);
+}
